9bd494df66079d1ae2993d76ab3fc6730e29a396
Ryan McDonough <rhinomcd@gmail.com>
2016-04-21 15:37:13 -0700

    adding data

---
+++ app/data/items.ts
@@ -0,0 +1,29 @@
+import {Item} from '../components/home/model/item.ts';
+
+export var ITEMS: Item[] = [
+        {
+            'name': 'Nature Rune',
+            'id': '561',
+            'price': '0'
+        },
+        {
+            'name': 'Cosmic Rune',
+            'id': '564',
+            'price': '0'
+        },
+         {
+            'name': 'Law Rune',
+            'id': '563',
+            'price': '0'
+        },
+         {
+            'name': 'Death Rune',
+            'id': '560',
+            'price': '0'
+        },
+         {
+            'name': 'Astral Rune',
+            'id': '9075',
+            'price': '0'
+        }
+    ];
