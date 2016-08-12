b7c3728dbdf51c55e2764d0d9ae00ed1ee962ca4
Ryan McDonough <rhinomcd@gmail.com>
2016-08-12 15:41:00 -0700

    progress on settings styling

--- tools/config.ts
+++ tools/config.ts
@@ -48,6 +48,7 @@ export const NPM_DEPENDENCIES = [
   { src: 'angular2/bundles/http.min.js', inject: 'libs', dest: LIB_DEST },
 
   { src: 'bootstrap/dist/css/bootstrap.min.css', inject: true, dest: CSS_DEST },
+  { src: 'https://fonts.googleapis.com/css?family=Roboto', inject:true, dest: CSS_DEST},
 
   { src: 'cerialize/dist/serialize.js', inject: true, dest: LIB_DEST }
 ];
